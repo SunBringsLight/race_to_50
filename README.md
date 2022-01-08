236d9f0b181bd80e3ef5d6a2ec20f53dc9ad9308
Sunshine Balingit <sunbringslight@gmail.com>
2022-01-08 09:38:00 -0800

    add play_game function to run through game and output winner

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # race_to_50
 Race To 50 is a simple game where the players roll a die until they get at least 50 dots total. The winner is whomever has the lowest number of rolls taken to reach the goal. This project is created in response to CodeAcademy's Full Stack Career Path course in creating a first project to practice JavaScript syntax.
 
+Note: I understand I can have each player roll one at a time until one of them reaches a total dot count of 50, but I wanted practice using objects. Thank you for allowing me to practice it even though there is a more efficient way of doing this.
+
 # Breakdown of Project
 
 ## class
@@ -12,7 +14,6 @@ To store data of each player
 - get dots: funct for game to see how many dots player has
 - get rolls: funct for game to see how many rolls play has taken
 - set name: set player's name
-- set rolls: sets roll count value to total rolls
 - roll die: acts as player rolling a die and adds the value to total dots rolled
 - increment_rolls: funct for player to increment roll count
 
@@ -20,6 +21,7 @@ To store data of each player
 ## functions
 - generate random number: randomly generates value between 1-6
 - find_winner: identifies player with least amount of rolls
+- play_game: takes in number of players wanting to play. Initiates game based on number of players, runs through the game, and outputs the player
 
 
 # Steps
--- race_game.js
+++ race_game.js
@@ -19,33 +19,28 @@ const player = (name) => {
         get dots() {
             return this._total_dots;
         },
+
         //see how many rolls play has taken
         get rolls() {
             return this._total_rolls;
         },
+
         //set player's name
         set name(name) {
             this._name = name;
         },
-/*
-        //adds roll value to total dots
-        set dots(num) {
-            this._total_dots += num;
-        },
-*/
-        set rolls(num) {
-          this._total_rolls = num;
-        },
+
         //funct for player to roll die
         roll_die() {
             this._total_dots += generate_random_number();
         },
+
         //funct for player to increment roll count
         increment_rolls() {
             this._total_rolls++;
         }
-    }
-}
+    };
+};
 
 /***************************************************************
 ****************************************************************
@@ -56,7 +51,6 @@ const player = (name) => {
 //function generate_random_number: randomly generates value between 1-6 to reflect player rolling a die
 const generate_random_number = () => {
     const roll_value = Math.floor(Math.random() * 6) + 1;
-    console.log(`roll die value: ${roll_value}`);
 
     return roll_value;
 };
@@ -65,38 +59,76 @@ const generate_random_number = () => {
 //function find_winner: identifies player with least amount of rolls and returns player's name
 const find_winner = players_arr => {
 
-  let min_player_index = 0;
+  //array to hold index of player(s) with lowest roll count
+    let min_player_index = [0];
 
-  for (let player=1; player < players_arr.length; player++) {
-    if (players_arr[player].rolls < players_arr[min_player_index].rolls) {
-      min_player_index = player;
-    }
+  //loop through each player and compare roll counts
+    for (let player=1; player < players_arr.length; player++) {
+        //if current player's roll count is less than current minimum roll count, then store index as only player with minimum roll count
+        if (players_arr[player].rolls < players_arr[min_player_index[0]].rolls) {
+            min_player_index = [player];
+        }
+        //else if the roll count is the same, keep all indexes that have the minimum roll count
+        else if (players_arr[player].rolls == players_arr[min_player_index[0]].rolls) {
+            min_player_index.push(player);
+        };
   };
   
-  console.log(`The winner is ${players_arr[min_player_index].name}!`);
+  //print who has minimum roll count as the winner(s)
+  if (min_player_index.length > 1) {
+    console.log('The winners are:');
+    for (let player in min_player_index) {
+      console.log(`\t${players_arr[min_player_index[player]].name}!`);
+    };
+  } else {
+    console.log(`The winner is ${players_arr[min_player_index[0]].name}!`);
+  };
   
 };
 
 
+//function play_game: initiates and runs through game based on how many players it was told were playing
+const play_game = total_players => {
+    //initialize winners
+    //create players
+    let players = [];
+    for (let i=0; i<total_players; i++) {
+        let temp_name = 'Player ' + (i+1);
+        let new_player = player(temp_name);
+        players.push(new_player);
+    };
 
-//initialize winners
-//create players
-let players = [];
-for (let i=0; i<5; i++) {
-  let temp_name = 'Player' + (i+1);
-  let new_player = player(temp_name);
-  players.push(new_player);
-}
-players[0].rolls = 5;
-players[1].rolls = 7;
-players[2].rolls = 1;
-players[3].rolls = 89;
-players[4].rolls = 10;
+    //for each player, roll die until they reach 50
+    for (let player in players) {
+        console.log(`${players[player].name}'s turn to start rolling!`);
 
-//print each player's rolls
-for (let player in players) {
-    console.log(`player ${players[player].name} roll value: ${players[player].rolls}`);
-}
+        do {
+            console.log(`\tPlayer ${players[player].name} current dots value: ${players[player].dots}.\n\tPlayer rolls die!`);
 
-//find winner
-find_winner(players);
+            //tally total dots received and how many rolls done
+            players[player].roll_die();
+            players[player].increment_rolls();
+
+        } while (players[player].dots < 50);
+
+        console.log(`${players[player].name} ends with a total dots value of ${players[player].dots}.\n`);
+    };
+    
+    //print each player's roll count
+    for (let player in players) {
+        console.log(`player ${players[player].name} roll value: ${players[player].rolls}`);
+    };
+    
+    //find winner
+    find_winner(players);
+};
+
+
+/***************************************************************
+****************************************************************
+**********              MAIN               *********************
+****************************************************************
+***************************************************************/
+
+//start game
+play_game(5);
